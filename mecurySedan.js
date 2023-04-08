1cffd799ce23df343432ecdca04de809a416e566
Rashonda Edwards <rashondaedwards@aol.com>
2023-04-08 16:08:58 -0700

    homework

--- mecurySedan.js
+++ mecurySedan.js
@@ -4,3 +4,55 @@ const VehicleModule = require("./vehicleBaseClass")
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
+
+class Car extends VehicleModule {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        this.maxPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+    loadPassenger(num) {
+        if (this.passenger < this.maxPassengers) {
+            if ((num + this.passenger) <= this.maxPassengers) {
+                this.passenger = num;
+                return this.passenger;               
+            } else {
+                console.log(`${this.model} ${this.make} not have enough space to take all passengers.`);
+
+            }
+        } else {
+            console.log(`${this.model} ${this.make} is full`);
+        }
+    }
+    //copied this from vehicle.js
+    start() {
+        if (this.fuel > 0) {
+            return this.started = true;
+            console.log("engine started...!!!");
+        } else {
+            return this.started = false;
+            console.log("engine cannot start...");
+        }
+    }
+
+    scheduleService(mileage) {
+        if (this.mileage > 30000) {            
+            this.scheduleService = true
+            return this.scheduleService;                       
+        }
+    }
+    
+}
+
+
+let myCar = new Car('mercury', 'rad_sedan', '2002', 'white', 50000)
+
+myCar.loadPassenger(5)
+myCar.start()
+myCar.scheduleService()
+
+console.log(myCar)
